dbf5938e96a32242b56abea9f81ab53a6c01df68
Ivan Avila <ivelaval@gmail.com>
2024-04-14 01:04:14 -0500

    Commander utility was added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
-const hello = (message: string) => {};
+import { Command } from "commander";
+import { version } from "../package.json";
 
-console.log("Let's go -----> ");
+const program = new Command();
+
+program
+  .name("string-util")
+  .description("CLI to some JavaScript string utilities")
+  .version(version);
+
+program
+  .command("split")
+  .description("Split a string into substrings and display as an array")
+  .argument("<string>", "string to split")
+  .option("--first", "display just the first substring")
+  .option("-s, --separator <char>", "separator character", ",")
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(str.split(options.separator, limit));
+  });
+
+program.parse(process.argv);
